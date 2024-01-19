1bee9fffc89501596551e7e696afe72f29cfd153
Cy Jay Herrera <herreracyjay24@gmail.com>
2024-01-19 10:00:15 +0800

    feat(project): Added the use of list of questions

--- lib/main.dart
+++ lib/main.dart
@@ -26,14 +26,13 @@ class QuizPage extends StatefulWidget {
 
 class _QuizPageState extends State<QuizPage> {
 
-  List<Icon> scoreKeeper = [
-    Icon(
-      Icons.check,
-      color: Colors.green,),
-    Icon(
-      Icons.close,
-      color: Colors.red,),
+  List<Icon> scoreKeeper = [];
+  List<String> questions = [
+    'You can lead a cow down stairs but not up stairs.',
+    'Approximately one quarter of human bones are in the feet.',
+    'A slug\'s blood is green.'
   ];
+  int tracker = 0;
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -46,7 +45,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                questions[tracker],
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -72,6 +71,7 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 setState(() {
+                  tracker++;
                   scoreKeeper.add(Icon(Icons.check, color: Colors.green,));
                 });
 
@@ -96,6 +96,7 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 setState(() {
+                  tracker++;
                   scoreKeeper.add(Icon(Icons.close, color: Colors.red,));
                 });
 
